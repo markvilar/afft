52c059f0d84778b4973e3825e333107e0d9284c7
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2024-09-12 17:32:23 +0200

    Remove runtime package and deprecated message export task / CLI.

--- src/afft/services/sirius/__init__.py
+++ src/afft/services/sirius/__init__.py
@@ -1,8 +1,24 @@
 """Package for message processing functionality for AUV Sirius."""
 
-from .data_parsers import parse_image_message
+from .message_parsers import (
+    parse_message_header,
+    parse_seabird_ctd_message,
+    parse_aanderaa_ctd_message,
+    parse_ecopuck_message,
+    parse_image_message,
+    parse_parosci_pressure_message,
+    parse_teledyne_dvl_message,
+    parse_lq_modem_message,
+    parse_evologics_modem_message,
+    parse_micron_sonar_message,
+    parse_obstacle_avoidance_sonar_message,
+    parse_battery_message,
+    parse_thruster_message,
+    MESSAGE_TYPE_TO_PARSER,
+)
 
-from .data_types import (
+
+from .message_types import (
     AuvMessageHeader,
     ImageCaptureMessage,
     SeabirdCTDMessage,
@@ -16,22 +32,6 @@ from .data_types import (
     OASonarMessage,
     BatteryMessage,
     ThrusterMessage,
-)
-
-from .data_parsers import (
-    parse_message_header,
-    parse_seabird_ctd_message,
-    parse_aanderaa_ctd_message,
-    parse_ecopuck_message,
-    parse_parosci_pressure_message,
-    parse_teledyne_dvl_message,
-    parse_lq_modem_message,
-    parse_evologics_modem_message,
-    parse_micron_sonar_message,
-    parse_obstacle_avoidance_sonar_message,
-    parse_battery_message,
-    parse_thruster_message,
-    MESSAGE_TYPE_TO_PARSER,
 )
 
 
