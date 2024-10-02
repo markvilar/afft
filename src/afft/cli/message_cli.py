bca25a8369543ed678a2296b959fc9fabd371a29
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2024-10-02 10:03:26 +0200

    Patch imports in message CLI.

--- src/afft/cli/message_cli.py
+++ src/afft/cli/message_cli.py
@@ -1,4 +1,4 @@
-"""Module for CLI for working with message data."""
+"""Module for message processing CLI."""
 
 from pathlib import Path
 from typing import Optional
@@ -7,9 +7,9 @@ import click
 
 from ..io import read_config, read_lines
 from ..services.sirius.message_interfaces import Message
-from ..services.sirius.message_set import (
-    MessageSet,
-    build_message_set,
+from ..services.sirius.message_protocol import (
+    MessageProtocol,
+    build_message_protocol,
     parse_message_lines,
 )
 from ..utils.log import logger
@@ -17,24 +17,34 @@ from ..utils.log import logger
 
 @click.group()
 @click.pass_context
-def message_services(context: click.Context) -> None:
-    """CLI group for message processing services."""
+def message_group(context: click.Context) -> None:
+    """CLI group for invoking message processing tasks."""
     context.ensure_object(dict)
 
 
-@message_services.command()
+@message_group.command()
 @click.argument("source", type=click.Path(exists=True))
+@click.argument("destination", type=click.Path(exists=True))
 @click.argument("config", type=click.Path(exists=True))
-@click.option("--database", type=str)
-def parse_messages(source: str, config: str, database: Optional[str] = None) -> None:
+@click.option("--prefix", type=str, help="Prefix for exported data groups")
+def parse_messages(
+    source: str, 
+    destination: str, 
+    config: str, 
+    prefix: Optional[str] = None,
+) -> None:
     """CLI action for parsing messages from a source."""
+
+    # TODO: Based on the destination - create / load destination endpoint
 
     config: dict = read_config(Path(config)).unwrap()
     lines: list[str] = read_lines(Path(source)).unwrap()
 
-    message_set: MessageSet = build_message_set(config.get("message_maps"))
+    message_protocol: MessageProtocol = build_message_protocol(config.get("message_maps"))
 
-    parsed_topics: dict[str, Message] = parse_message_lines(lines, message_set)
+    parsed_topics: dict[str, Message] = parse_message_lines(lines, message_protocol)
+
+    # TODO: Merge topics with the same message types?
 
     for topic, messages in parsed_topics.items():
         logger.info(f"Topic: {topic}, messages: {len(messages)}")
