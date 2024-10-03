2b83ae7a1ab107ffe89be51fd162509842a3a9a6
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2024-10-03 11:56:12 +0200

    Add CLI command to parse and insert messages in databases.

--- src/afft/io/sql/endpoint.py
+++ src/afft/io/sql/endpoint.py
@@ -1,20 +1,42 @@
-"""Module for SQL database endpoint."""
+"""Module for SQL database endpoints."""
+
+from typing import Optional
 
 import sqlalchemy as db
+import dotenv as env
 
-from dotenv import dotenv_values
+from ...utils.result import Ok, Err, Result
+
+Endpoint = db.Engine
 
 
-values = dotenv_values("/home/martin/dev/afft/.env")
+USER_KEY: str = "DB_USER"
+PASSWORD_KEY: str = "DB_PASSWORD"
 
 
-url: db.engine.URL = db.engine.URL.create(
-    drivername="postgresql",
-    database="acfr_auv_revisits",
-    host="localhost",
-    port=5432,
-    username=values.get("PG_USER"),
-    password=values.get("PG_PASSWORD"),
-)
+def create_endpoint(
+    database: str, 
+    host: str, 
+    port: int, 
+    drivername: str="postgresql",
+) -> Result[Endpoint, str]:
+    """Creates a connection to a SQL database."""
 
-engine: db.Engine = db.create_engine(url)
+    username: Optional[str] = env.dotenv_values().get(USER_KEY)
+    password: Optional[str] = env.dotenv_values().get(PASSWORD_KEY)
+
+    if not username:
+        return Err(f"missing environment value: '{USER_KEY}'")
+    if not password:
+        return Err(f"missing environment value: '{PASSWORD_KEY}'")
+
+    url: db.engine.URL = db.engine.URL.create(
+        drivername=drivername,
+        database=database,
+        host=host,
+        port=port,
+        username=username,
+        password=password,
+    )
+
+    return Ok(db.create_engine(url))
