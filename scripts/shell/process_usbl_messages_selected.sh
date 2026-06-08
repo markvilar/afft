a0f560d12959f0b2d3eda82f432324f684e16bb0
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2026-06-08 11:59:18 +0200

    Add shell script to process USBL messages for the selected deployments.

--- scripts/shell/process_usbl_messages_selected.sh
+++ scripts/shell/process_usbl_messages_selected.sh
@@ -4,13 +4,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIG_DIR="${SCRIPT_DIR}/../../config"
 
 SOURCE_DIR="/data/exos_01/acfr_messages_v2_parsed"
-OUTPUT_DIR="/data/exos_01/acfr_messages_v4_telemetry_processed"
+OUTPUT_DIR="/data/exos_01/acfr_messages_v4_processed"
 
 # --------------------------------------------------------------------------------------
 # Site 1
 # --------------------------------------------------------------------------------------
 
-# NOTE: Test USBL processing
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/qdch0ftq_20100428_020202_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/qdch0ftq_20100428_020202_pressure_parosci.csv" \
@@ -18,6 +18,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "qdch0ftq_20100428_020202"
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/qdch0ftq_20110415_020103_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/qdch0ftq_20110415_020103_pressure_parosci.csv" \
@@ -25,13 +26,16 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "qdch0ftq_20110415_020103"
 
+# Verdict: ignore extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/qdch0ftq_20120430_002423_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/qdch0ftq_20120430_002423_pressure_parosci.csv" \
   --output-file "${OUTPUT_DIR}/qdch0ftq_20120430_002423_usbl_tracklink.csv" \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
-  --deployment "qdch0ftq_20120430_002423"
+  --deployment "qdch0ftq_20120430_002423" \
+  --ignore-extrinsics
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/qdch0ftq_20130406_023610_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/qdch0ftq_20130406_023610_pressure_parosci.csv" \
@@ -39,12 +43,11 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "qdch0ftq_20130406_023610"
 
-exit 0
-
 # --------------------------------------------------------------------------------------
 # Site 2
 # --------------------------------------------------------------------------------------
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/qdchdmy1_20110416_005411_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/qdchdmy1_20110416_005411_pressure_parosci.csv" \
@@ -52,13 +55,16 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "qdchdmy1_20110416_005411"
 
+# Verdict: ignore extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/qdchdmy1_20120501_071203_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/qdchdmy1_20120501_071203_pressure_parosci.csv" \
   --output-file "${OUTPUT_DIR}/qdchdmy1_20120501_071203_usbl_tracklink.csv" \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
-  --deployment "qdchdmy1_20120501_071203"
+  --deployment "qdchdmy1_20120501_071203" \
+  --ignore-extrinsics
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/qdchdmy1_20130406_081713_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/qdchdmy1_20130406_081713_pressure_parosci.csv" \
@@ -66,13 +72,10 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "qdchdmy1_20130406_081713"
 
-exit 0
-
-# NOTE: Change to Evologics USBL
-uv run afft sensors process-tracklink-usbl-from-messages \
-  --usbl-file "${SOURCE_DIR}/qdchdmy1_20170525_234624_usbl_tracklink.csv" \
-  --pressure-file "${SOURCE_DIR}/qdchdmy1_20170525_234624_pressure_parosci.csv" \
-  --output-file "${OUTPUT_DIR}/qdchdmy1_20170525_234624_usbl_tracklink.csv" \
+# Verdict: use extrinsics (Evologics S2C 18/34)
+uv run afft sensors process-evologics-usbl \
+  --usbl-file "${SOURCE_DIR}/qdchdmy1_20170525_234624_usbl_evologics.csv" \
+  --output-file "${OUTPUT_DIR}/qdchdmy1_20170525_234624_usbl_evologics.csv" \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "qdchdmy1_20170525_234624"
 
@@ -80,6 +83,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
 # Site 3
 # --------------------------------------------------------------------------------------
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r23685bc_20100605_021022_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r23685bc_20100605_021022_pressure_parosci.csv" \
@@ -87,6 +91,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "r23685bc_20100605_021022"
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r23685bc_20120530_233021_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r23685bc_20120530_233021_pressure_parosci.csv" \
@@ -94,6 +99,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "r23685bc_20120530_233021"
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r23685bc_20140616_225022_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r23685bc_20140616_225022_pressure_parosci.csv" \
@@ -105,6 +111,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
 # Site 4
 # --------------------------------------------------------------------------------------
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r29mrd5h_20090612_225306_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r29mrd5h_20090612_225306_pressure_parosci.csv" \
@@ -112,6 +119,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "r29mrd5h_20090612_225306"
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r29mrd5h_20110612_033752_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r29mrd5h_20110612_033752_pressure_parosci.csv" \
@@ -119,6 +127,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "r29mrd5h_20110612_033752"
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r29mrd5h_20130611_002419_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r29mrd5h_20130611_002419_pressure_parosci.csv" \
@@ -130,6 +139,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
 # Site 5
 # --------------------------------------------------------------------------------------
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r7jjskxq_20101023_210332_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r7jjskxq_20101023_210332_pressure_parosci.csv" \
@@ -137,6 +147,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "r7jjskxq_20101023_210332"
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r7jjskxq_20121013_060425_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r7jjskxq_20121013_060425_pressure_parosci.csv" \
@@ -144,6 +155,7 @@ uv run afft sensors process-tracklink-usbl-from-messages \
   --deployment-configs "${CONFIG_DIR}/deployment_configs_selected.toml" \
   --deployment "r7jjskxq_20121013_060425"
 
+# Verdict: use extrinsics
 uv run afft sensors process-tracklink-usbl-from-messages \
   --usbl-file "${SOURCE_DIR}/r7jjskxq_20131022_004934_usbl_tracklink.csv" \
   --pressure-file "${SOURCE_DIR}/r7jjskxq_20131022_004934_pressure_parosci.csv" \
