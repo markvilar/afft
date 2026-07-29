df2d6b00e96d2b2539c00b4aab23aee7621c6736
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2026-07-29 20:56:23 +0200

    Group deployment package re-exports by module

--- src/afft/deployment/__init__.py
+++ src/afft/deployment/__init__.py
@@ -1,29 +1,31 @@
 """Package for AUV deployment configuration."""
 
-from .descriptor import DeploymentDescriptor as DeploymentDescriptor
-from .descriptor import DeploymentFileSection as DeploymentFileSection
-from .descriptor import DeploymentSensor as DeploymentSensor
-from .descriptor import DeploymentSensorSection as DeploymentSensorSection
-from .descriptor import DeploymentSystemSection as DeploymentSystemSection
 from .descriptor import (
+    DeploymentDescriptor as DeploymentDescriptor,
+    DeploymentFileSection as DeploymentFileSection,
+    DeploymentSensor as DeploymentSensor,
+    DeploymentSensorSection as DeploymentSensorSection,
+    DeploymentSystemSection as DeploymentSystemSection,
     DeploymentTelemetrySection as DeploymentTelemetrySection,
+    PlatformIdentity as PlatformIdentity,
+    SensorExtrinsics as SensorExtrinsics,
+    SensorIdentity as SensorIdentity,
 )
-from .descriptor import PlatformIdentity as PlatformIdentity
-from .descriptor import SensorExtrinsics as SensorExtrinsics
-from .descriptor import SensorIdentity as SensorIdentity
-from .files import DeploymentFiles as DeploymentFiles
-from .files import collect_deployment_files as collect_deployment_files
-from .loader import load_deployment_config as load_deployment_config
+from .files import (
+    DeploymentFiles as DeploymentFiles,
+    collect_deployment_files as collect_deployment_files,
+)
 from .loader import (
+    load_deployment_config as load_deployment_config,
     read_deployment_descriptors as read_deployment_descriptors,
-)
-from .loader import read_deployment_info as read_deployment_info
-from .loader import (
+    read_deployment_info as read_deployment_info,
     write_deployment_descriptors as write_deployment_descriptors,
+    write_deployment_info as write_deployment_info,
 )
-from .loader import write_deployment_info as write_deployment_info
-from .types import DeploymentConfig as DeploymentConfig
-from .types import DeploymentInfo as DeploymentInfo
-from .types import DeploymentMetadata as DeploymentMetadata
-from .types import TopsideUsblModemConfig as TopsideUsblModemConfig
-from .types import UsblUncertaintyProfile as UsblUncertaintyProfile
+from .types import (
+    DeploymentConfig as DeploymentConfig,
+    DeploymentInfo as DeploymentInfo,
+    DeploymentMetadata as DeploymentMetadata,
+    TopsideUsblModemConfig as TopsideUsblModemConfig,
+    UsblUncertaintyProfile as UsblUncertaintyProfile,
+)
