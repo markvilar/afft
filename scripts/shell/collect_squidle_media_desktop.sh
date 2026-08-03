b53b6845d301449f11c0dd385f33d9acefb67a48
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2026-08-03 11:12:50 +0200

    Add bash scripts to collect Squidle media records and images.

---
+++ scripts/shell/collect_squidle_media_desktop.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_DIR="$(realpath -m ${SCRIPT_DIR}/../../config)"
+
+OUTPUT_DIR="/data/exos_01/acfr_squidle_collections"
+
+
+echo "Script dir: ${SCRIPT_DIR}"
+echo "Config dir: ${CONFIG_DIR}"
+
+
+uv run afft tasks collect-squidle-media \
+  --deployments-file "${CONFIG_DIR}/acfr_deployments.toml" \
+  --output-dir "${OUTPUT_DIR}"
